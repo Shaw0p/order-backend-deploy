7b87450070e0db209b89621b2ff276b73041965f
Shaw0p <assh23ise@cmrit.ac.in>
2025-08-03 20:16:16 +0530

    orderserverapp

--- src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import com.example.orderservice.config.AwsConfig;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AwsConfig.class)
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
