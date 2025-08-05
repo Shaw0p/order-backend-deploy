e01b652ed0126fceb3fd0ebe4f1c7de88f719b7b
Shaw0p <assh23ise@cmrit.ac.in>
2025-08-05 09:07:43 +0530

    Update CorsConfig.java

--- src/main/java/com/example/orderservice/config/CorsConfig.java
+++ src/main/java/com/example/orderservice/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
                     .allowedOrigins(
                         "https://order-management-frontend.vercel.app",
                         "https://order-management-frontend-99mb-mf4dv5ha9-shaw0ps-projects.vercel.app",
-                        "https://order-management-frontend-99mb-1if5wjivj-shaw0ps-projects.vercel.app"
+                        "https://order-management-frontend-99mb-1if5wjivj-shaw0ps-projects.vercel.app",
+                        "https://order-management-frontend-99mb-gbn310j0g-shaw0ps-projects.vercel.app"
                     )
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") 
                     .allowedHeaders("*")
